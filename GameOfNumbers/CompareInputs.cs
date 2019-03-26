de91f5fd0c3191b4669f23857376f3e10526082e
harjot kaur gosal <harjotkaur9439@gmail.com>
2019-03-27 11:48:53 +1300

    added compare method

---
+++ GameOfNumbers/CompareInputs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNumbers
+{
+    class CompareInputs
+    {
+
+        /// <summary>
+        /// This method is comparing the actual number with the inputed number to check whether user has guess the correct number or how close his guess to correct number
+        /// </summary>
+        /// <param name="inputedNumber"></param>
+        /// <param name="actualNumber"></param>
+        /// <returns></returns>
+        public string IsTheSame(int inputedNumber, int actualNumber)
+        {
+            string result=null;
+            if (inputedNumber == actualNumber)
+            {
+                result = "Success!!!";
+            }
+            else
+            {
+               if(inputedNumber==(actualNumber+1) || inputedNumber == (actualNumber - 1))
+                {
+                    result = "Warmer!!!";
+                }
+                else if (inputedNumber < (actualNumber + 5) && inputedNumber > (actualNumber - 5))
+                {
+                    result = "Warm!!!";
+                }
+                else if (inputedNumber < (actualNumber + 10) && inputedNumber > (actualNumber - 10))
+                {
+                    result = "Cold!!!";
+                }
+                else
+                {
+                    result = "Coldest!!!";
+                }
+            }
+            return result;
+        }
+    }
+}
