f6e7ef5960499aa8423b8da7cc0e95a12c13088a
MRozza <Mohamed.a.rozza@gmail.com>
2019-03-27 21:56:51 +1300

    Added UI Integrated application sections

--- GameOfNumbers/Form1.Designer.cs
+++ GameOfNumbers/Form1.Designer.cs
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbDifficulty = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pbTrials = new System.Windows.Forms.ProgressBar();
+            this.cbInput = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblPrize = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDifficulty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbDifficulty
+            // 
+            this.tbDifficulty.Location = new System.Drawing.Point(483, 17);
+            this.tbDifficulty.Maximum = 3;
+            this.tbDifficulty.Minimum = 1;
+            this.tbDifficulty.Name = "tbDifficulty";
+            this.tbDifficulty.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tbDifficulty.Size = new System.Drawing.Size(56, 188);
+            this.tbDifficulty.TabIndex = 0;
+            this.tbDifficulty.Value = 1;
+            this.tbDifficulty.Scroll += new System.EventHandler(this.tbDifficulty_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Difficulty";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(531, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1 - 25";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "1 - 50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "1 - 100";
+            // 
+            // pbTrials
+            // 
+            this.pbTrials.Location = new System.Drawing.Point(156, 212);
+            this.pbTrials.Maximum = 5;
+            this.pbTrials.Name = "pbTrials";
+            this.pbTrials.Size = new System.Drawing.Size(303, 23);
+            this.pbTrials.Step = 5;
+            this.pbTrials.TabIndex = 5;
+            // 
+            // cbInput
+            // 
+            this.cbInput.FormattingEnabled = true;
+            this.cbInput.Location = new System.Drawing.Point(191, 151);
+            this.cbInput.Name = "cbInput";
+            this.cbInput.Size = new System.Drawing.Size(121, 24);
+            this.cbInput.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(153, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "What\'s your guess?";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(334, 151);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 8;
+            this.btnOk.Text = "Ok!";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblPrize
+            // 
+            this.lblPrize.AutoSize = true;
+            this.lblPrize.Location = new System.Drawing.Point(307, 17);
+            this.lblPrize.Name = "lblPrize";
+            this.lblPrize.Size = new System.Drawing.Size(24, 17);
+            this.lblPrize.TabIndex = 9;
+            this.lblPrize.Text = "25";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(178, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "You\'re playing for:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(331, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(621, 251);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblPrize);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbInput);
+            this.Controls.Add(this.pbTrials);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDifficulty);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Game of Numbers!";
+            ((System.ComponentModel.ISupportInitialize)(this.tbDifficulty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar tbDifficulty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar pbTrials;
+        private System.Windows.Forms.ComboBox cbInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblPrize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- GameOfNumbers/Form1.cs
+++ GameOfNumbers/Form1.cs
@@ -12,9 +12,50 @@ namespace GameOfNumbers
 {
     public partial class Form1 : Form
     {
+        int _numberOfTries = 0;
         public Form1()
         {
             InitializeComponent();
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 25; i++)
+            {
+                numbers.Add(i);
+            }
+            cbInput.DataSource = numbers;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (_numberOfTries > 5)
+            {
+                MessageBox.Show("Sorry, you have exceeded the allowed number of tries!");
+                return;
+            }
+
+            _numberOfTries++;
+            pbTrials.Value = _numberOfTries;
+            pbTrials.BackColor = Color.Red;
+
+        }
+
+        private void tbDifficulty_Scroll(object sender, EventArgs e)
+        {
+            var tb = (TrackBar)sender;
+            switch (tb.Value)
+            {
+                case 1:
+                    lblPrize.Text = "25$";
+                    break;
+                case 2:
+                    lblPrize.Text = "50$";
+                    break;
+                case 3:
+                    lblPrize.Text = "100$";
+                    break;
+                default:
+                    lblPrize.Text = "25$";
+                    break;
+            }
         }
     }
 }
