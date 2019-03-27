ff16f505730071f845106501a2a9f7be8f020c7a
harjot kaur gosal <harjotkaur9439@gmail.com>
2019-03-27 20:46:44 +1300

    added CompareInputs class

--- GameOfNumbers/CompareInputs.cs
+++ GameOfNumbers/CompareInputs.cs
@@ -10,38 +10,36 @@ namespace GameOfNumbers
     {
 
         /// <summary>
-        /// This method is comparing the actual number with the inputed number to check whether user has guess the correct number or how close his guess to correct number
+        /// This method is comparing the actual number with the inputed number to check whether user has guess the correct number or how close his guess to the actual number
         /// </summary>
         /// <param name="inputedNumber"></param>
         /// <param name="actualNumber"></param>
         /// <returns></returns>
         public string IsTheSame(int inputedNumber, int actualNumber)
         {
-            string result=null;
             if (inputedNumber == actualNumber)
             {
-                result = "Success!!!";
+                return "Success!!!";
             }
             else
             {
                if(inputedNumber==(actualNumber+1) || inputedNumber == (actualNumber - 1))
                 {
-                    result = "Warmer!!!";
+                    return "Warmer!!!";
                 }
                 else if (inputedNumber < (actualNumber + 5) && inputedNumber > (actualNumber - 5))
                 {
-                    result = "Warm!!!";
+                    return "Warm!!!";
                 }
                 else if (inputedNumber < (actualNumber + 10) && inputedNumber > (actualNumber - 10))
                 {
-                    result = "Cold!!!";
+                    return "Cold!!!";
                 }
                 else
                 {
-                    result = "Coldest!!!";
+                    return "Coldest!!!";
                 }
             }
-            return result;
         }
     }
 }
