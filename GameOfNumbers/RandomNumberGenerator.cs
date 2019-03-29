fe93cca7f143242d6821a814b267da17be1390d4
MRozza <m.rozza.eg@gmail.com>
2019-03-29 14:41:57 +1300

    Code Review for Jun's work

--- GameOfNumbers/RandomNumberGenerator.cs
+++ GameOfNumbers/RandomNumberGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// review by Mohamed
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,34 +26,42 @@ namespace GameOfNumbers
     /// </summary>
     class RandomNumberGenerator
     {
+        // Global variables should start with _
         private HashSet<int> numberSets = new HashSet<int>();
+        // we don't neet these variables, you can pass them instead to generate method.
+        // i.e. Generate(int minValue, int maxValue)
         private readonly int minValue;
         private readonly int maxValue;
+
+        // These are not needed since they are handled in the main class (Form1.cs)
         public readonly int maxTrials;
         private int trials = 0;
-
+        // Not needed if we're gonna use just Generate method
         public RandomNumberGenerator(int minValue, int maxValue, int maxTrials)
         {
             this.minValue = minValue;
             this.maxValue = maxValue;
             this.maxTrials = maxTrials;
         }
-
+        // not needed
         public int GetMaxTrial()
         {
             return maxTrials;
         }
-
+        // not needed
         public int GetCurrentTrials()
         {
             return trials;
         }
-
+        // nover used, should be used inside Generate
         public void Clear()
         {
             numberSets.Clear();
             trials = 0;
         }
+
+
+        //the summery specifies min and max parameters but they are not there 
 
         /// <summary>
         /// Generate random number between minValue and maxValue.
@@ -63,7 +73,7 @@ namespace GameOfNumbers
         public int Generate()
         {
             int value = -1;
-
+            // no need for this check since it will performed in the main class
             if (trials++ == maxTrials)
             {
                 throw new InvalidOperationException("Cannot try more than max trials.");
